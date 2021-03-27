ab9ad733fb2f571a0547f966cd860819d787ffe7
chitti428 <81452251+chitti428@users.noreply.github.com>
2021-03-27 07:07:57 +0530

    submitted day1 assignment

---
+++ Assignment1/assignment1.js
@@ -0,0 +1,5 @@
+var element1=console.log(document.getElementsByTagName('div'));
+var element2=console.log(document.getElementsByClassName('chitti'));
+var element3=console.log(document.getElementById('chitti babu'));
+var element4=(document.getElementsByClassName('chitti'));
+element4[0].innerText="web developer";
